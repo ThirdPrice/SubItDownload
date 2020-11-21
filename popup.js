b7b12a78bf62bf7e20545758ea78857c7bfbb051
ThirdPrice <59074660+ThirdPrice@users.noreply.github.com>
2020-11-20 19:04:52 -0500

    Update popup.js
    Update for UI and error messages

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {  
     var checkPageButton = document.getElementById('mainButt');  
+    x = document.getElementById("result");
     checkPageButton.addEventListener('click', function() {  
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-                document.getElementById("results").innerText = response.response;
-            });
+            if (tabs[0].url != "https://account.subitup.com/employee/default.aspx") {
+                x.style.display = "inline-block";
+                x.innerText = "You must be on SubItUp.com!";
+            }
+            else {
+                chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+
+                    x.style.display = "inline-block";
+                    if (response.response == "success") {
+                        x.style.backgroundColor = "#67CF3E";
+                        x.innerText = "Download Sucessful!";
+                    }
+                    else {
+                        x.innerText = "Switch to week view!";
+                    }
+                });
+            }
         }); 
     }, false);  
 }, false);  
